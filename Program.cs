146c6a4b5fe6ddd33569408aa8bd244e1d189817
Luiz-code-dev <programadorlesd@gmail.com>
2021-03-14 14:59:31 -0300

    Bootcamp .NET Developer

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int numerodevezes =10;
+
+           for (int i=0; i < numerodevezes; i++)
+           {
+               Console.WriteLine($"Bem Vindo ao curso de .NET {i}");
+           } 
+        }
+    }
+}
